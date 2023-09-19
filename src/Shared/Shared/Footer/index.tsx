c18f3ae9385a81bf0c49ace4e4a1329d3621bdca
sureshkumara6688 <130357890+sureshkumara6688@users.noreply.github.com>
2023-09-19 09:40:19 +0530

    Add files via upload

---
+++ src/Shared/Shared/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo7 from "../Images/Footer.svg"
+import './index.css'
+export default function Footer() {
+  return (
+    <div>
+        <div className='containerimg'>
+    <img src={logo7} alt='name' /> 
+    </div>
+    </div>
+  )
+}
